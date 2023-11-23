6f28983c1faf826c2b89e155199b2e3ebc61bb7c
sergio2448 <84557725+sergio2448@users.noreply.github.com>
2023-11-22 22:03:21 -0500

    Add expo cli config

---
+++ challenge1/MyFirstApp/App.tsx
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello, Coder!</Text>
+      <Text>Welcome to the world of React Native</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 10.5,
+  },
+
+  text: {
+    fontWeight: 'bold',
+    fontSize: 50,
+  },
+});

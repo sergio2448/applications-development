504407b28791f472a69263bc1f60912acc5a21d4
sergio2448 <84557725+sergio2448@users.noreply.github.com>
2023-12-04 23:26:41 -0500

    Add first components

---
+++ challenge2/OptimizedList/src/components/InputComponent.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const InputComponent = () => {
+  return (
+    <View>
+      <Text>InputComponent</Text>
+    </View>
+  );
+};
+
+export default InputComponent;
+
+const styles = StyleSheet.create({});
---
+++ challenge2/OptimizedList/src/components/ModalComponent.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const ModalComponent = () => {
+  return (
+    <View>
+      <Text>ModalComponent</Text>
+    </View>
+  );
+};
+
+export default ModalComponent;
+
+const styles = StyleSheet.create({});
